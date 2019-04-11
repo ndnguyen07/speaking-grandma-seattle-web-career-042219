529d5de773dd1b6cc112bc800a057ba937136fd7
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-11 06:40:30 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!".upcase
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma == "".upcase
-  puts "NO, NOT SINCE 1938!"
-else speak_to_grandma == "".downcase
-  puts "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
 end
-
-
